2f7268dd1ceb268fc10ac8c902427584fa009923
BarysTsibets <barystsibets@gmail.com>
2023-07-21 13:20:12 -0700

    Refactoring

--- Code_wars task 8.py
+++ Code_wars task 8.py
@@ -4,31 +4,21 @@ is even, return the middle 2 characters. """
 
 
 def get_middle(s):
+    # logic for Not Odd length
     if len(s) % 2 == 0:
-        first_index = len(s)//2
+        first_index = len(s) // 2
         second_index = first_index + 1
-        print(f'{first_index} first index')
-        print(f'{second_index} second index')
-        print(s[first_index - 1: first_index + 1])
-        return s[first_index - 1: first_index + 1]
-        print(f'{s} is not odd')
+        return s[first_index - 1: second_index]
 
-    #logic for 1 character
+    # logic for Odd length
+    elif len(s) % 2 != 0:
+        middle_char = len(s) // 2
+        return s[middle_char]
+
+    # logic for 1 character
     elif len(s) == 1:
         return s
-        print(f'{s} is middle character')
-
-    elif len(s)%2 != 0:
-        middle_char = len(s)//2
-        print(s[middle_char])
-        return s[middle_char]
-        print(f'{s[middle_char]}')
-        print(f'{s} is odd')
+        # print(f'{s} is middle character')
 
 
-get_middle('hel')
-
-# listt = [1,3,7,"this",3,"that", 7]
-# # use // Floor Division
-# middle = len(listt)//2
-# print(middle)
+get_middle('hello')
